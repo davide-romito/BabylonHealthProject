8e24b2e3bf2c1b6d596db7220b2b2a7d850a5dd4
davideromito <dalir86@gmail.com>
2016-06-30 23:03:26 +0200

    New method to convert a set of string

--- src/main/java/it/davideromito/crawler/converter/JSONConverter.java
+++ src/main/java/it/davideromito/crawler/converter/JSONConverter.java
@@ -2,6 +2,9 @@ package it.davideromito.crawler.converter;
 
 import com.google.gson.Gson;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Utility class, to convert object in Json format
  */
@@ -16,5 +19,19 @@ public final class JSONConverter {
     public static String toJSON(Object obj){
         Gson gson = new Gson();
         return gson.toJson(obj);
+    }
+
+    /**
+     * Given a set, return a set of string in the JSON format of that object
+     * @param set any type of objects
+     * @return set of Json format String
+     */
+    public static Set<String> toJSON(Set set){
+        Set<String> stringSet = new HashSet<String>();
+        Gson gson = new Gson();
+        for (Object obj : set){
+            stringSet.add(gson.toJson(obj));
+        }
+        return stringSet;
     }
 }
