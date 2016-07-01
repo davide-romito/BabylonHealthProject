f294e0541b01ba3a205d81f79e9e263f2ed43233
davide-romito <dalir86@gmail.com>
2016-07-01 17:06:38 +0200

    Create WT.java

---
+++ src/main/java/it/davideromito/crawler/model/WT.java
@@ -0,0 +1,43 @@
+package it.davideromito.crawler.model;
+
+/**
+ * Created by dromito on 01/07/2016.
+ */
+public class WT {
+    private String ti;
+    private String cgN;
+    private String cgS;
+    private String sv;
+
+    public String getTi() {
+        return ti;
+    }
+
+    public void setTi(String ti) {
+        this.ti = ti;
+    }
+
+    public String getCgN() {
+        return cgN;
+    }
+
+    public void setCgN(String cgN) {
+        this.cgN = cgN;
+    }
+
+    public String getCgS() {
+        return cgS;
+    }
+
+    public void setCgS(String cgS) {
+        this.cgS = cgS;
+    }
+
+    public String getSv() {
+        return sv;
+    }
+
+    public void setSv(String sv) {
+        this.sv = sv;
+    }
+}
