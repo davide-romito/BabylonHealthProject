abd9c0970f911eaa1479e54f0b87b6e2b354a5bc
davide-romito <dalir86@gmail.com>
2016-07-01 17:26:23 +0200

    Update JsoupParsing.java

--- src/main/java/it/davideromito/crawler/parsing/jsoup/JsoupParsing.java
+++ src/main/java/it/davideromito/crawler/parsing/jsoup/JsoupParsing.java
@@ -1,3 +1,4 @@
+
 package it.davideromito.crawler.parsing.jsoup;
 
 import it.davideromito.crawler.model.Page;
@@ -20,9 +21,27 @@ public class JsoupParsing implements Parsing {
         page.setTitle(doc.title());
         page.setDescription(doc.getElementsByAttributeValue(NAME,"description").attr(CONTENT));
         page.setKeywords(doc.getElementsByAttributeValue(NAME,"keywords").attr(CONTENT));
-        page.setDcTitle(doc.getElementsByAttributeValue(NAME,"DC.title").attr(CONTENT));
-        page.setDcDescription(doc.getElementsByAttributeValue(NAME,"DC.description").attr(CONTENT));
-        //TODO extend
+        page.getDc().setTitle(doc.getElementsByAttributeValue(NAME,"DC.title").attr(CONTENT));
+        page.getDc().setDescription(doc.getElementsByAttributeValue(NAME,"DC.description").attr(CONTENT));
+        page.getDc().setSubject(doc.getElementsByAttributeValue(NAME,"DC.subject").attr(CONTENT));
+        page.getDc().setDateIssued(doc.getElementsByAttributeValue(NAME,"DC.date.issued").attr(CONTENT));
+        page.getDc().setCoverage(doc.getElementsByAttributeValue(NAME,"DC.coverage").attr(CONTENT));
+        page.getDc().setCreator(doc.getElementsByAttributeValue(NAME,"DC.creator").attr(CONTENT));
+        page.getDc().setFormat(doc.getElementsByAttributeValue(NAME,"DC.format").attr(CONTENT));
+        page.getDc().setLanguage(doc.getElementsByAttributeValue(NAME,"DC.language").attr(CONTENT));
+        page.getDc().setIdentifier(doc.getElementsByAttributeValue(NAME,"DC.identifier").attr(CONTENT));
+        page.getDc().setPublisher(doc.getElementsByAttributeValue(NAME,"DC.publisher").attr(CONTENT));
+        page.getDc().setRights(doc.getElementsByAttributeValue(NAME,"DC.rights").attr(CONTENT));
+        page.getEgms().setAccessibility(doc.getElementsByAttributeValue(NAME,"eGMS.accessibility").attr(CONTENT));
+        page.getWt().setTi(doc.getElementsByAttributeValue(NAME,"WT.ti").attr(CONTENT));
+        page.getDcSext().setRealUrl(doc.getElementsByAttributeValue(NAME,"DCSext.RealUrl").attr(CONTENT));
+        page.getWt().setCgN(doc.getElementsByAttributeValue(NAME,"WT.cg_n").attr(CONTENT));
+        page.getWt().setCgS(doc.getElementsByAttributeValue(NAME,"WT.cg_s").attr(CONTENT));
+        page.getDcSext().setServer(doc.getElementsByAttributeValue(NAME,"DCSext.Server").attr(CONTENT));
+        page.getWt().setSv(doc.getElementsByAttributeValue(NAME,"WT.sv").attr(CONTENT));
+        page.getDcSext().setBmSection1(doc.getElementsByAttributeValue(NAME,"DCSext.BM_Section1").attr(CONTENT));
+        page.getDcSext().setBmSection2(doc.getElementsByAttributeValue(NAME,"DCSext.BM_Section2").attr(CONTENT));
+        page.getDcSext().setBmSection3(doc.getElementsByAttributeValue(NAME,"DCSext.BM_Section3").attr(CONTENT));
         return page;
     }
 }
