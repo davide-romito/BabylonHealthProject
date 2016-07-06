f528ad877cc0fe61535031467bc3c8e33e3e4ea4
davideromito <dalir86@gmail.com>
2016-07-06 22:26:17 +0200

    Crawler Changes

--- src/main/java/it/davideromito/crawler/Crawler.java
+++ src/main/java/it/davideromito/crawler/Crawler.java
@@ -1,5 +1,6 @@
 package it.davideromito.crawler;
 
+import it.davideromito.Constant;
 import it.davideromito.crawler.converter.JSONConverter;
 import it.davideromito.crawler.model.Page;
 import it.davideromito.crawler.retrieve.page.PagesRetriever;
@@ -9,42 +10,68 @@ import it.davideromito.crawler.save.FileSave;
 import it.davideromito.crawler.save.Save;
 
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Created by davideromito on 28/06/16.
  */
 public class Crawler {
-    public static void main(String[] args) throws Exception {
+    public void generateKnowledge(){
+        long start = System.nanoTime();
+        Set<Page> pages = extractInformations();
+        saveInformations(pages);
+        System.out.println("Knowledge generation in "+((System.nanoTime()-start)/Math.pow(10,9))+ " seconds");
+    }
 
+
+    protected Set<Page> extractInformations(){
         Retrieve retrieve = new RetrieveImpl();
 
-        String basePage = "http://www.nhs.uk";
-        String rootPage = basePage + "/Conditions/Pages/";
-        String homepage = rootPage + "hub.aspx";
-
         System.out.println("START RETRIEVE link from HOMEPAGE");
-        Set<String> set = retrieve.retrieveSetOfLinks(homepage, "<a href=\"BodyMap.aspx");
+        Set<String> set = new HashSet<String>();
+        try {
+            set = retrieve.retrieveSetOfLinks(Constant.HOMEPAGE, "<a href=\"BodyMap.aspx");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("END RETRIEVE link from HOMEPAGE");
 
         System.out.println("START RETRIEVE link from VARIOUS PAGES");
         Set<String> setLink = new HashSet<String>();
         for (String s : set) {
-            setLink.addAll(retrieve.retrieveSetOfLinks(rootPage+s, "<a href=\"/conditions" ));
+            try {
+                setLink.addAll(retrieve.retrieveSetOfLinks(Constant.ROOT_PAGE+s, "<a href=\"/conditions" ));
+            } catch (IOException e) {
+                System.out.println(Constant.ROOT_PAGE+s);
+                e.printStackTrace();
+            }
         }
         System.out.println("END RETRIEVE link from VARIOUS PAGES");
 
         System.out.println("START RETRIEVE INFORMATION from VARIOUS PAGES");
-        PagesRetriever pr = new PagesRetriever(basePage);
+        PagesRetriever pr = new PagesRetriever(Constant.BASE_PAGE);
         Set<Page> pages = pr.retrievePages(setLink);
         System.out.println("END RETRIEVE INFORMATION from VARIOUS PAGES");
+        return pages;
+    }
 
+    protected void saveInformations(Set pages){
         System.out.println("START SAVE PHASE");
-        Save save = new FileSave("CRAWLER");
-        save.saveSet(JSONConverter.toJSON(pages));
+        Save save;
+        try {
+            save = new FileSave(Constant.FILE_NAME);
+            save.saveSet(JSONConverter.toJSON(pages));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("END SAVE PHASE");
+    }
 
-
+    public static void main(String[] args) {
+        Crawler c = new Crawler();
+        c.generateKnowledge();
     }
 }
