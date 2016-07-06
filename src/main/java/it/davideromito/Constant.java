33dd8e26e1ab944c6c87fb5079d0655c944fffb4
davideromito <dalir86@gmail.com>
2016-07-06 22:26:45 +0200

    Constant

---
+++ src/main/java/it/davideromito/Constant.java
@@ -0,0 +1,12 @@
+package it.davideromito;
+
+/**
+ * Class to store all the constant information
+ */
+public interface Constant {
+    String BASE_PAGE = "http://www.nhs.uk";
+    String ROOT_PAGE = BASE_PAGE + "/Conditions/Pages/";
+    String HOMEPAGE = ROOT_PAGE + "hub.aspx";
+
+    String FILE_NAME = "FILE";
+}
